004f4315400c6fa178dd1f60d9237e01c1d8f857
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2023-01-11 10:27:30 +0200

    reseting  the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { AboutComponent } from './components/about/about.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PComponent } from './components/p/p.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    ContactComponent,
-    AboutComponent,
-    PostsComponent,
-    PComponent,
-    NotfoundComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
